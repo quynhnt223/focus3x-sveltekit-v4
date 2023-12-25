b4d285fd6e38b8642409e829e2b17e987d6bdf0e
quynhnguyen7489 <88937118+quynhnguyen7489@users.noreply.github.com>
2023-12-25 10:39:21 +0700

    production v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import cloudfare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	kit: {
+		adapter: cloudfare()
+	}
+};
+
+/* export default config;
+
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+/* const config = {
 	kit: {
 		adapter: adapter()
 	}
 };
 
-export default config;
+export default config; */
